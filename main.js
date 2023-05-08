05d4dd8cdb95db443cae1f369d2823322b3ad9b4
Adam Goldberg <112718049+agold95@user.noreply.github.com>
2023-05-08 13:32:22 -0400

    added evaluate function

--- main.js
+++ main.js
@@ -7,34 +7,57 @@ function subtract(a, b) {
 }
 
 function divide(a, b) {
-    return (a / b)
+    return (a / b);
 }
 
 function multiply(a, b) {
-    return (a * b)
+    return (a * b);
 }
 
 function operate(a, operator, b) {
-    a = Number(a)
-    b = Number(b)
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
         case '+':
-          return add(a, b)
+          return add(a, b);
         case '-':
-          return subtract(a, b)
+          return subtract(a, b);
         case 'x':
-          return multiply(a, b)
+          return multiply(a, b);
         case '÷':
           if (b === 0) return null
-          else return divide(a, b)
+          else return divide(a, b);
         default:
-          return null
+          return null;
       }
 }
 
 function operatorConversion(operator) {
-    if (operator === '/') return '÷'
-    if (operator === '*') return 'x'
-    if (operator === '-') return '-'
-    if (operator === '+') return '+'
+    if (operator === '/') return '÷';
+    if (operator === '*') return 'x';
+    if (operator === '-') return '-';
+    if (operator === '+') return '+';
   }
+
+function round(num) {
+    return Math.round(num * 1000) / 1000;
+}
+
+const lastOperation = document.getElementsByClassName('last-display')
+const currentOperation = document.getElementsByClassName('current-display');
+
+let operationType = null;
+let screenClear = false;
+
+function evaluate() {
+    if (operationType === null || screenClear) {
+        return;
+    }
+    if (operationType === '÷' && currentOperation.textContent === '0') {
+        alert("You cannot divide by 0");
+        return;
+    }
+    b = currentOperation.textContent;
+    currentOperation.textContent = round(operate(a, operationType, b));
+    lastOperation.textContent = `${a}${operationType}${b}`;
+}
