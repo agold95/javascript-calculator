378a64e5751252ffa38d611d503f0dcf3817a232
Adam Goldberg <112718049+agold95@user.noreply.github.com>
2023-05-08 10:58:32 -0400

    mathmatic functionality implemented

---
+++ main.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function divide(a, b) {
+    return (a / b)
+}
+
+function multiply(a, b) {
+    return (a * b)
+}
+
+function operate(a, operator, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+          return add(a, b)
+        case '-':
+          return subtract(a, b)
+        case 'x':
+          return multiply(a, b)
+        case '÷':
+          if (b === 0) return null
+          else return divide(a, b)
+        default:
+          return null
+      }
+}
