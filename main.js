e40a4a362a8561c00940ebbfb8ada4e6d7c685f0
Adam Goldberg <112718049+agold95@user.noreply.github.com>
2023-05-08 15:55:52 -0400

    added button functionality

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const lastOperation = document.getElementById('last-display');
+const currentOperation = document.getElementById('current-display');
+
+let operationType = null;
+let a = '';
+let b = '';
+let shouldDisplayClear = false;
+
 function add(a, b) {
     return (a + b);
 }
@@ -43,14 +53,28 @@ function round(num) {
     return Math.round(num * 1000) / 1000;
 }
 
-const lastOperation = document.getElementsByClassName('last-display')
-const currentOperation = document.getElementsByClassName('current-display');
+function resetDisplay() {
+    currentOperation.textContent = '';
+    shouldDisplayClear = false;
+}
 
-let operationType = null;
-let screenClear = false;
+function appendNum(num) {
+    if (currentOperation.textContent === '0' || shouldDisplayClear) {
+        resetDisplay();
+    }
+        currentOperation.textContent += num;
+}
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => appendNum(button.textContent))
+)
+
+operatorButtons.forEach((button) => 
+    button.addEventListener('click', () => startOperation(button.textContent))
+)
 
 function evaluate() {
-    if (operationType === null || screenClear) {
+    if (operationType === null || shouldDisplayClear) {
         return;
     }
     if (operationType === '÷' && currentOperation.textContent === '0') {
@@ -59,5 +83,27 @@ function evaluate() {
     }
     b = currentOperation.textContent;
     currentOperation.textContent = round(operate(a, operationType, b));
-    lastOperation.textContent = `${a}${operationType}${b}`;
+    lastOperation.textContent = a + operationType + b;
+}
+
+function startOperation(operator) {
+    if (operationType !== null) {
+        evaluate();
+    }
+    a = currentOperation.textContent;
+    operationType = operator;
+    lastOperation.textContent = a + operationType;
+    shouldDisplayClear = true;
+}
+
+function clear() {
+    currentOperation.textContent = '0';
+    lastOperation.textContent = '';
+    a = '';
+    b = '';
+    operationType = null;
+}
+
+function deleteNum() {
+    currentOperation.textContent = currentOperation.textContent.toString().slice(0, -1);
 }
